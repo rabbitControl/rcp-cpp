6644c838d2f99d1d0567b3928aea877c6a01dbec
i-n-g-o <ingo.randolf@servus.at>
2018-06-07 16:55:54 +0200

    move implementation into cpp

--- src/parameterfactory.h
+++ src/parameterfactory.h
@@ -20,46 +20,7 @@ namespace rcp {
             return std::make_shared<RangeParameter<T> >(id);
         }
 
-        static ParameterPtr createRangeParameter(int16_t parameter_id, datatype_t type_id) {
-
-            switch (type_id) {
-            case DATATYPE_INT8:
-                return RangeParameter<int8_t>::create(parameter_id);
-
-            case DATATYPE_UINT8:
-                return RangeParameter<uint8_t>::create(parameter_id);
-
-            case DATATYPE_INT16:
-                return RangeParameter<int16_t>::create(parameter_id);
-
-            case DATATYPE_UINT16:
-                return RangeParameter<uint16_t>::create(parameter_id);
-
-            case DATATYPE_INT32:
-                return RangeParameter<int32_t>::create(parameter_id);
-
-            case DATATYPE_UINT32:
-                return RangeParameter<uint32_t>::create(parameter_id);
-
-            case DATATYPE_INT64:
-                return RangeParameter<int64_t>::create(parameter_id);
-
-            case DATATYPE_UINT64:
-                return RangeParameter<uint64_t>::create(parameter_id);
-
-            case DATATYPE_FLOAT32:
-                return RangeParameter<float>::create(parameter_id);
-
-            case DATATYPE_FLOAT64:
-                return RangeParameter<double>::create(parameter_id);
-
-            default:
-                // error...?
-                break;
-            }
-
-            return nullptr;
-        }
+        static ParameterPtr createRangeParameter(int16_t parameter_id, datatype_t type_id);
     };
 
 }

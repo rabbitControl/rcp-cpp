f77e1dc97d599c02d1e0f9b9dbd3f2d78f2a7440
Ingo Randolf <ingo.randolf@servus.at>
2020-06-19 20:10:03 +0200

    add friends and value update cb to IParameter

--- src/iparameter.h
+++ src/iparameter.h
@@ -138,6 +138,9 @@ namespace rcp {
         template<typename, datatype_t, td_types> friend class TypeDefinition;
         friend class GroupParameter;
         friend class ParameterManager;
+        friend class ParameterParser;
+        friend class ParameterClient;
+        friend class ParameterServer;
 
     protected:
         virtual void setDirty() = 0;
@@ -164,6 +167,11 @@ namespace rcp {
         virtual void setValue(const T& value) = 0;
         virtual bool hasValue() const = 0;
         virtual void clearValue() = 0;
+
+        // update callbacks
+        virtual const std::function< void ( T& )>& addValueUpdatedCb(std::function< void(T&) >& func) = 0;
+        virtual void removeValueUpdatedCb(const std::function< void(T&) >& func) = 0;
+        virtual void clearValueUpdatedCb() = 0;
     };   
 
     class IElementParameter

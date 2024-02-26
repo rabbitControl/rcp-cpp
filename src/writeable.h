4c70f108a3a6317cab6c7f3d51a88ed8bf2ac085
i-n-g-o <ingo.randolf@servus.at>
2024-02-26 16:31:42 +0800

    update license header

--- src/writeable.h
+++ src/writeable.h
@@ -7,7 +7,7 @@
 *
 * This file is part of rabbitcontrol for c++.
 *
-* Written by Ingo Randolf, 2018-2023
+* Written by Ingo Randolf, 2018-2024
 *
 * This Source Code Form is subject to the terms of the Mozilla Public
 * License, v. 2.0. If a copy of the MPL was not distributed with this
